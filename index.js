08710632c9422252b99a264d4267d1d43824546e
Ahsan2001 <ahsan2sabir@gmail.com>
2021-03-13 20:19:15 +0500

    added files

---
+++ index.js
@@ -0,0 +1,74 @@
+/*-------------------------------Question # 1 Start------------------------------- */
+
+// var car = {
+//     Trim:	"L 4.0 TFSI quattro",
+//     Engine:	"4.0L Twin-Turbo V8 Gas",
+//     Transmission:	"8-Speed Automatic",
+//     Drivetrain:	"All-Wheel Drive",
+//     Price:	"$130,900",
+//     bodyStyle: "4-door sedan",
+//     address: function() {
+//         return "House # 7, Str # 3, Pitember Street, Garden West, Karachi "
+//       },
+//     OwnerInfo: function(){
+//         return "Owner Name is Ahsan Living in karachi"
+//     }
+// }
+
+/*-------------------------------Question # 1 End------------------------------  */
+
+
+
+/*-------------------------------Question # 2 Start------------------------------- */
+function (firstName,lastName,age) {  
+    console.log(`This  is ${firstName}  ${lastName} & my age is ${age}`);
+}
+ firstName = "Ahsan";
+lastName = "Sabir";
+ age = 19
+ 
+setTimeout(message, 3000);
+
+
+
+/*-------------------------------Question # 2 End------------------------------  */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*-------------------------------Question # 3 Start------------------------------- */
+// var password = prompt(`Enter Your Password`);
+
+// if (password.length > 12 && password.includes("@", "#", "$", "&", "*", "%", "!", "^", "?")) {
+//     document.write("Your Password is Strong")
+// }
+
+// else if (password.length > 8 || password.length > 5 && password.includes("@", "#", "$", "&", "*", "%", "!", "^", "?")) {
+//     document.write("Your Password is Strong enough")
+// }
+
+// else {
+//     document.write("Your Password is too Week")
+// }
+
+// document.write(`<br /><br />Your Password is : ${password}`);
+
+/*-------------------------------Question # 3 End------------------------------  */
