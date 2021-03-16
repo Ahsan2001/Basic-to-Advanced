2f78e3ec40f3e42b2df80bf919b00e6b9c176fcc
Ahsan2001 <ahsan2sabir@gmail.com>
2021-03-16 13:12:51 +0500

    update files

--- index.js
+++ index.js
@@ -103,3 +103,32 @@
 // });
 
 /*-------------------------------Question # 6 End------------------------------  */
+
+
+
+/*-------------------------------Question # 9 Start------------------------------  */
+
+
+// String is a Primitive Data Type. String is used for storing Data example letter,number,symbols,text etc
+// Syntax of String is " " Example are given in below
+// e.g:
+//    var result = "My name is " + name + " My email is " + email + 
+
+// Template literals are String literals allow you to multi line string. 
+// Syntax of template string is backtic `` Example are given in below
+// e.g:
+//    var result = `My name is ${name} My email is ${email}`
+
+
+// Method of String
+
+// var name = "AhsanSabir"
+// console.log(name.toUpperCase())      //convert All string in Capital Letter
+// console.log(name.toLowerCase())      //convert All string in Small Letter
+// console.log(name[2])                 //it give the value of No.2 index 
+// console.log(name.length)             //it give you the total number of character
+// console.log(name.slice(1,5))         //it give you the value between  index 1 to 5 start from 1 end in 4 
+// console.log(name.substr(1,5))        //it give you the value between  index 1 to 5 start from 1 end in 5
+
+
+/*-------------------------------Question # 9 End------------------------------  */
